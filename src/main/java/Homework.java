8ffcf9fe0602243fe44523c36bc890778e92dd42
klaud <@Klkwiecien>
2023-07-25 22:30:53 +0200

    nowa praca

---
+++ src/main/java/Homework.java
@@ -0,0 +1,9 @@
+public class Homework {
+    public int convertTemperature(int number) {
+        int Farenheit = 2*number;
+        return Farenheit; }
+
+    public static void main(String[] args)  {
+        System.out.println("30 stopni Celsius: " + 2*30 );
+    }
+    }
