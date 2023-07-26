c11ddd28dde23985723cc2185e4adcac8c582643
klaud <@Klkwiecien>
2023-07-26 18:48:11 +0200

    nowa praca dwa

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -1,4 +1,4 @@
-public class Homework {
+class Farenheit {
     public int convertTemperature(int number) {
         int Farenheit = 2*number;
         return Farenheit; }
@@ -7,3 +7,18 @@ public class Homework {
         System.out.println("30 stopni Celsius: " + 2*30 );
     }
     }
+  class Volvo {
+          String[] cars = {"Volvo", "BMW", "Ford"};
+      public static void main(String[] args)  {
+          boolean Volvo = true;
+          System.out.println(Volvo);
+      }
+      static class kolejne {
+          public static void main(String[] args)  {
+          String[] Cars = {"Volvo", "BMW", "Ford"};
+          int BMW = 2;
+          int Ford = 3;
+          int Volvo = 1;
+
+              System.out.println("BMW,Ford"); }}}
+
